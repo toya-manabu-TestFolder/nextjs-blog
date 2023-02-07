920b323638526b84d715aa6ec5cc6413c5299aa4
Toya Manabu <manabu.toya@rakus-partners.co.jp>
2023-02-07 22:31:19 +0900

    test2

---
+++ components/layout.tsx
@@ -0,0 +1,81 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import Link from "next/link";
+import utilStyles from "/home/toya_manabu/src/github.com/toya-manabu-TestFolder/nextjs-blog/src/styles/utils.module.css";
+export const siteTitle = "Next.js Sample Website";
+
+export default function Layout({
+  // childrenは指定されていない他ページ
+  children,
+  //   homeはindex.tsx
+  home,
+}: {
+  children: any;
+  home: boolean;
+}) {
+  console.log(children);
+  const name = "Your Name";
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {/* homeページかそうじゃないかで */}
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          // ↓HOMEじゃなかった時に画像とタイトルのサイズを変更
+          <>
+            <Link href="/">
+              <Image
+                priority
+                src="/images/profile.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt={name}
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {/* ↓HOMEじゃなかったら出現 */}
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/pages/posts/first-post.tsx
@@ -0,0 +1,17 @@
+import Layout from "/home/toya_manabu/src/github.com/toya-manabu-TestFolder/nextjs-blog/components/layout";
+import Head from "next/head";
+import Link from "next/link";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>最初の投稿</title>
+      </Head>
+      <h1>最初の投稿</h1>
+      <h2>
+        <Link href="/">トップページに戻る</Link>
+      </h2>
+    </Layout>
+  );
+}
