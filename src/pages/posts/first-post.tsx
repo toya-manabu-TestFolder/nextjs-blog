f7597ea999269eefa92c7236d1994b1f25be3767
Toya Manabu <manabu.toya@rakus-partners.co.jp>
2023-02-08 22:30:11 +0900

    test7

--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 import Layout from "/home/toya_manabu/src/github.com/toya-manabu-TestFolder/nextjs-blog/components/layout";
 import Head from "next/head";
-=======
->>>>>>> main
 import Link from "next/link";
 
 export default function FirstPost() {
   return (
-<<<<<<< HEAD
     <Layout>
       <Head>
         <title>最初の投稿</title>
@@ -17,13 +13,5 @@ export default function FirstPost() {
         <Link href="/">トップページに戻る</Link>
       </h2>
     </Layout>
-=======
-    <>
-      <h1>最初の投稿</h1>
-      <h2>
-        <Link href="/">トップページに戻る</Link>
-      </h2>
-    </>
->>>>>>> main
   );
 }
